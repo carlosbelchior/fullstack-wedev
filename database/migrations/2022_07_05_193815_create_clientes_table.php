2c1e0d1aec229dd0535ffbcf914f0e1fc8aba9b4
Carlos Belchior <carlosbelchiorsilva@gmail.com>
2022-07-05 19:46:04 -0300

    Migrations

--- database/migrations/2022_07_05_193815_create_clientes_table.php
+++ database/migrations/2022_07_05_193815_create_clientes_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->string('cpf', 11)->unique();
+            $table->string('email')->nullable()->unique();
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('clientes');
     }
 };
--- database/migrations/2022_07_05_194122_create_pedidos_table.php
+++ database/migrations/2022_07_05_194122_create_pedidos_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('pedidos', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->foreignId('cliente_id')->constrained('clientes');
+            $table->date('data_pedido');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('pedidos');
     }
 };
