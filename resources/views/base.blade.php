11988adb07c2163d6719e161a6879c781bc81319
Carlos Belchior <carlosbelchiorsilva@gmail.com>
2022-07-09 22:59:58 -0300

    Adicionado familia de fontes

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -12,14 +12,16 @@
     <!-- Scripts -->
     <script src="http://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="http://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="http://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <body>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
     <div id="app"></div>
     @vite('resources/js/app.js')
 </body>
